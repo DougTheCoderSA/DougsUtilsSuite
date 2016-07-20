94acbd47a707b9ca73ed8bf9867456c7774e159d
Douglas Crole <dcrole@businessmodeling.com>
2016-07-20 13:54:02 +0200

    2016-07-20 SnapshotCompare

---
+++ SnapshotCompare/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace SnapshotCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.LVStoredProcs1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBSnapshot1 = new System.Windows.Forms.ComboBox();
+            this.LVStoredProcs2 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.CBSnapshot2 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusText});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(694, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusText
+            // 
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(679, 17);
+            this.StatusText.Spring = true;
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(694, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.LVStoredProcs1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.CBSnapshot1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.LVStoredProcs2);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.CBSnapshot2);
+            this.splitContainer1.Size = new System.Drawing.Size(694, 403);
+            this.splitContainer1.SplitterDistance = 331;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // LVStoredProcs1
+            // 
+            this.LVStoredProcs1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LVStoredProcs1.CheckBoxes = true;
+            this.LVStoredProcs1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.LVStoredProcs1.FullRowSelect = true;
+            this.LVStoredProcs1.HideSelection = false;
+            this.LVStoredProcs1.Location = new System.Drawing.Point(6, 30);
+            this.LVStoredProcs1.MultiSelect = false;
+            this.LVStoredProcs1.Name = "LVStoredProcs1";
+            this.LVStoredProcs1.Size = new System.Drawing.Size(322, 370);
+            this.LVStoredProcs1.TabIndex = 2;
+            this.LVStoredProcs1.UseCompatibleStateImageBehavior = false;
+            this.LVStoredProcs1.View = System.Windows.Forms.View.Details;
+            this.LVStoredProcs1.SelectedIndexChanged += new System.EventHandler(this.LVStoredProcs1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Schema";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ProcName";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Created";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Modified";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Snapshot 1";
+            // 
+            // CBSnapshot1
+            // 
+            this.CBSnapshot1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CBSnapshot1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CBSnapshot1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CBSnapshot1.FormattingEnabled = true;
+            this.CBSnapshot1.Location = new System.Drawing.Point(70, 3);
+            this.CBSnapshot1.Name = "CBSnapshot1";
+            this.CBSnapshot1.Size = new System.Drawing.Size(258, 21);
+            this.CBSnapshot1.TabIndex = 0;
+            this.CBSnapshot1.SelectedIndexChanged += new System.EventHandler(this.CBSnapshot1_SelectedIndexChanged);
+            // 
+            // LVStoredProcs2
+            // 
+            this.LVStoredProcs2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LVStoredProcs2.CheckBoxes = true;
+            this.LVStoredProcs2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.LVStoredProcs2.FullRowSelect = true;
+            this.LVStoredProcs2.HideSelection = false;
+            this.LVStoredProcs2.Location = new System.Drawing.Point(6, 30);
+            this.LVStoredProcs2.Name = "LVStoredProcs2";
+            this.LVStoredProcs2.Size = new System.Drawing.Size(341, 370);
+            this.LVStoredProcs2.TabIndex = 3;
+            this.LVStoredProcs2.UseCompatibleStateImageBehavior = false;
+            this.LVStoredProcs2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Schema";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "ProcName";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Created";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Modified";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Snapshot 2";
+            // 
+            // CBSnapshot2
+            // 
+            this.CBSnapshot2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CBSnapshot2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CBSnapshot2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CBSnapshot2.FormattingEnabled = true;
+            this.CBSnapshot2.Location = new System.Drawing.Point(70, 3);
+            this.CBSnapshot2.Name = "CBSnapshot2";
+            this.CBSnapshot2.Size = new System.Drawing.Size(277, 21);
+            this.CBSnapshot2.TabIndex = 0;
+            this.CBSnapshot2.SelectedIndexChanged += new System.EventHandler(this.CBSnapshot2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 449);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SQL Snapshot Compare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CBSnapshot1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CBSnapshot2;
+        private System.Windows.Forms.ListView LVStoredProcs1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ListView LVStoredProcs2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ToolStripStatusLabel StatusText;
+    }
+}
+
---
+++ SnapshotCompare/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KBCsv;
+
+namespace SnapshotCompare
+{
+    public partial class Form1 : Form
+    {
+        public string OutputFolder;
+        public List<string> SnapshotDescriptionList;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OutputFolder = "C:\\DataPersonal\\Projects\\DougsUtilsSuite\\zzSnapshotDB\\bin\\Debug\\Output";
+
+            SnapshotDescriptionList = new List<string>();
+
+            // Recurse into snapshot folders, get each snapshot's info.
+            string SnapshotInfoFileName = "SnapshotInfo.csv";
+            string CSVSnapshotInfoFilePath;
+            string[] SnapshotDirs;
+            SnapshotDirs = Directory.GetDirectories(OutputFolder);
+            foreach (string CurrentSnapshotDirectory in SnapshotDirs)
+            {
+                CSVSnapshotInfoFilePath = Path.Combine(CurrentSnapshotDirectory, SnapshotInfoFileName);
+
+                if (File.Exists(CSVSnapshotInfoFilePath))
+                {
+                    using (var streamReader = new StreamReader(CSVSnapshotInfoFilePath))
+                    using (var reader = new CsvReader(streamReader))
+                    {
+                        // the CSV file has a header record, so we read that first
+                        reader.ReadHeaderRecord();
+
+                        while (reader.HasMoreRecords)
+                        {
+                            var dataRecord = reader.ReadDataRecord();
+
+                            SnapshotDescriptionList.Add(dataRecord["SnapshotDescription"]);
+                        }
+                    }            
+                }
+            }
+
+
+            // Initialise the Snapshot dropdowns.
+            CBSnapshot1.Items.AddRange(SnapshotDescriptionList.ToArray());
+            CBSnapshot2.Items.AddRange(SnapshotDescriptionList.ToArray());
+        }
+
+        public void CompareSnapshots()
+        {
+            // Validate selected snapshots
+            if ((CBSnapshot1.SelectedIndex != -1) && (CBSnapshot2.SelectedIndex != -1))
+            {
+                if (CBSnapshot1.SelectedIndex == CBSnapshot2.SelectedIndex)
+                {
+                    MessageBox.Show("Cannot compare a snapshot to itself. Please select 2 different snapshots to compare.");
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            StatusText.Text = "Compare Beginning";
+
+            string Snapshot1Path, Snapshot2Path, SPList1, SPList2;
+            Snapshot1Path = CBSnapshot1.SelectedItem.ToString();
+            Snapshot1Path = Path.Combine(OutputFolder, Snapshot1Path);
+            Snapshot2Path = CBSnapshot2.SelectedItem.ToString();
+            Snapshot2Path = Path.Combine(OutputFolder, Snapshot2Path);
+            SPList1 = Path.Combine(Snapshot1Path, "StoredProcList.csv");
+            SPList2 = Path.Combine(Snapshot2Path, "StoredProcList.csv");
+
+            ListViewItem item;
+
+            LVStoredProcs1.Items.Clear();
+            LVStoredProcs2.Items.Clear();
+
+            // Populate List Views from CSV Stored Proc Lists
+            using (var streamReader = new StreamReader(SPList1))
+            using (var reader = new CsvReader(streamReader))
+            {
+                // the CSV file has a header record, so we read that first
+                reader.ReadHeaderRecord();
+
+                while (reader.HasMoreRecords)
+                {
+                    var dataRecord = reader.ReadDataRecord();
+
+                    item = new ListViewItem();
+                    //item.UseItemStyleForSubItems = false;
+                    item.Text = dataRecord["Schema"];
+                    item.SubItems.Add(dataRecord["ProcName"]);
+                    item.SubItems.Add(dataRecord["CreateDate"]);
+                    item.SubItems.Add(dataRecord["ModifyDate"]);
+                    LVStoredProcs1.Items.Add(item);
+                }
+            }
+
+            foreach (ColumnHeader column in LVStoredProcs1.Columns)
+            {
+                column.Width = -2;
+            }
+
+            using (var streamReader = new StreamReader(SPList2))
+            using (var reader = new CsvReader(streamReader))
+            {
+                // the CSV file has a header record, so we read that first
+                reader.ReadHeaderRecord();
+
+                while (reader.HasMoreRecords)
+                {
+                    var dataRecord = reader.ReadDataRecord();
+
+                    item = new ListViewItem();
+                    //item.UseItemStyleForSubItems = false;
+                    item.Text = dataRecord["Schema"];
+                    item.SubItems.Add(dataRecord["ProcName"]);
+                    item.SubItems.Add(dataRecord["CreateDate"]);
+                    item.SubItems.Add(dataRecord["ModifyDate"]);
+                    LVStoredProcs2.Items.Add(item);
+                }
+            }
+
+            foreach (ColumnHeader column in LVStoredProcs2.Columns)
+            {
+                column.Width = -2;
+            }
+
+            // Highlight modified dates of more recent procs
+            foreach (ListViewItem itemLeft in LVStoredProcs1.Items)
+            {
+                DateTime LeftLastModified, RightLastModified;
+                string LeftSchema, RightSchema;
+                string LeftProcName, RightProcName;
+                
+                // Get left side info
+                LeftSchema = itemLeft.Text;
+                LeftProcName = itemLeft.SubItems[1].Text;
+                LeftLastModified = Convert.ToDateTime(itemLeft.SubItems[3].Text);
+
+                foreach (ListViewItem itemRight in LVStoredProcs2.Items)
+                {
+                    // Get Right side info
+                    RightSchema = itemRight.Text;
+                    RightProcName = itemRight.SubItems[1].Text;
+                    RightLastModified = Convert.ToDateTime(itemRight.SubItems[3].Text);
+
+                    if ((LeftSchema.ToLower() == RightSchema.ToLower()) && (LeftProcName.ToLower() == RightProcName.ToLower()))
+                    {
+                        if (LeftLastModified > RightLastModified)
+                        {
+                            itemLeft.ForeColor = Color.Green;
+                        }
+                        if (LeftLastModified < RightLastModified)
+                        {
+                            itemRight.ForeColor = Color.Green;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CBSnapshot1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CompareSnapshots();
+        }
+
+        private void CBSnapshot2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CompareSnapshots();
+        }
+
+        private void OnSelectionChanged(ListView lv1, ListView lv2)
+        {
+            if (lv1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = lv1.SelectedItems[0];
+                
+            }
+        }
+
+        private void LVStoredProcs1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
